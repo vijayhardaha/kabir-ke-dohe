8c5513bb32ca24bd0a6c39a8744145e6a1d08db7
Vijay Hardaha <vijayhardaha@gmail.com>
2024-07-09 14:53:47 +0530

    feat(add): add build src code

---
+++ src/index.js
@@ -0,0 +1,86 @@
+import fs from "fs/promises";
+import path from "path";
+import axios from "axios";
+import {fileURLToPath} from "url";
+
+// Get the directory name of the current module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+/**
+ * Generates Markdown content with numbered entries.
+ *
+ * @param {Object[]} entries - Array of entry objects containing doha text.
+ * @param {number} startNum - The starting number for the entries.
+ * @return {string} - The generated Markdown content with numbered entries.
+ */
+const generateMarkdownContent = (entries, startNum) => {
+  return entries
+    .map((entry, index) => {
+      const entryIndex = startNum + index;
+      return `${entryIndex}. ${entry.doha.split("\n").join("\\\n") +`${entryIndex}।।`}`;
+    })
+    .join("\n\n---\n\n");
+};
+/**
+ * Pads a number with leading zeros.
+ *
+ * @param {number} number - The number to pad.
+ * @param {number} width - The desired width of the number string.
+ * @return {string} The padded number as a string.
+ */
+const padNumber = (number, width) => {
+  const numberStr = number.toString();
+  return numberStr.padStart(width, "0");
+};
+
+/**
+ * Creates markdown files from the given filtered data, with each file containing a specified number of entries.
+ *
+ * @param {Array<Object>} data - The filtered data to write to markdown files.
+ * @param {number} [entriesPerFile=50] - The number of entries per markdown file.
+ * @return {Promise<void>}
+ */
+const createMarkdownFiles = async (data, entriesPerFile = 50) => {
+  const docsDir = path.resolve(__dirname, "..", "collections");
+
+  // Ensure the docs directory exists
+  await fs.mkdir(docsDir, {recursive: true});
+
+  for (let i = 0; i < data.length; i += entriesPerFile) {
+    const entries = data.slice(i, i + entriesPerFile);
+    const startNum = i + 1;
+    const startNumber = padNumber(i + 1, 2);
+    const endNumber = padNumber(Math.min(i + entriesPerFile, data.length), 2);
+    const heading = `संत कबीर जी के दोहों का संग्रह — ${startNumber} to ${endNumber}`;
+    const content = `# ${heading}\n\n${generateMarkdownContent(entries, startNum)}`;
+    const fileName = `collection-${startNumber}-to-${endNumber}.md`;
+    const filePath = path.join(docsDir, fileName);
+
+    await fs.writeFile(filePath, content, "utf8");
+    console.log(`File created: ${path.basename(filePath)}`);
+  }
+};
+
+/**
+ * Main function to read data from a JSON file, filter by author, and generate markdown files.
+ *
+ * @return {Promise<void>}
+ */
+const main = async () => {
+  try {
+    // URL to fetch Kabir Ke Dohe API data
+    const apiUrl = "https://santo-ki-seekh.netlify.app/api/kabir-ke-dohe/";
+
+    // Fetch data from API endpoint
+    const response = await axios.get(apiUrl);
+    const jsonData = response.data;
+
+    await createMarkdownFiles(jsonData);
+  } catch (error) {
+    console.error("Error reading or processing data:", error);
+  }
+};
+
+// Run the main function
+main();
