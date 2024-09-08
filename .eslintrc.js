fe852301d67cf49002fda419c96f10e9570a2999
Vijay Hardaha <vijayhardaha@gmail.com>
2024-09-09 01:25:13 +0530

    chore(.estlint): setup eslint with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,40 @@
-// .eslintrc.js
 module.exports = {
 	root: true,
 	extends: [
 		"next",
 		"next/core-web-vitals",
 		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended",
 		"plugin:react/recommended",
 		"plugin:jsx-a11y/recommended",
+		"plugin:prettier/recommended", // Add Prettier integration
 	],
-	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		ecmaVersion: 2020,
-		sourceType: "module",
+	plugins: ["react", "jsx-a11y", "import", "prettier"],
+	env: {
+		browser: true, // Add browser environment for web applications.
+		node: true, // Add Node.js environment for server-side code.
+		es6: true, // Enable ES6 syntax.
 	},
-	plugins: ["react", "@typescript-eslint", "jsx-a11y", "import"],
+	parser: "@babel/eslint-parser",
+	parserOptions: {
+		ecmaVersion: 2024, // Set to latest ECMAScript version.
+		sourceType: "module",
+		ecmaFeatures: {
+			jsx: true, // Enable JSX syntax.
+		},
+	},
 	rules: {
 		// Customize ESLint rules here
-		"react/react-in-jsx-scope": "off", // Next.js does not require importing React in JSX files
-		"@typescript-eslint/explicit-module-boundary-types": "off", // Example customization
+		"react/react-in-jsx-scope": "off", // Next.js does not require importing React in JSX files.
+		"prettier/prettier": "error", // Enforce Prettier formatting as errors.
+		"react/no-unknown-property": [
+			"error",
+			{
+				ignore: [
+					"jsx", // Ignore the 'jsx' property in JSX.
+					"global", // Ignore the 'global' property in JSX.
+				],
+			},
+		],
 		"import/order": [
 			"error",
 			{
