123f75529a0f606dddb5cc599477301e9b047369
Vijay Hardaha <vijayhardaha@proton.me>
2026-06-03 15:59:04 +0530

    chore: update import path for QueryParameters
    - Changes import path from ./QueryParameters to ./__tests__/QueryParameters
    - Maintains all existing functionality

--- src/components/docs/index.tsx
+++ src/components/docs/index.tsx
@@ -1,6 +1,6 @@
 export { default as Introduction } from './Introduction';
 export { default as ApiEndpoints } from './ApiEndpoints';
-export { default as QueryParameters } from './QueryParameters';
+export { default as QueryParameters } from './__tests__/QueryParameters';
 export { default as ResponseFormat } from './ResponseFormat';
 export { default as ErrorResponse } from './ErrorResponse';
 export { default as Examples } from './Examples';
