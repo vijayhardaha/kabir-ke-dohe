79c5733564802168b5d9ea2d4a77f8e1e576d7c2
Vijay Hardaha <vijayhardaha@proton.me>
2026-06-17 04:08:28 +0530

    chore: remove puppeteer config file
    - Delete unused api/puppeteer.config.mjs
    - Let Puppeteer use its default config

--- api/puppeteer.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import("puppeteer").Configuration} */
-const config = {
-  // Download Chrome (default `skipDownload: false`).
-  chrome: { skipDownload: false },
-  // Download Firefox (default `skipDownload: true`).
-  firefox: { skipDownload: false },
-};
-
-export default config;
